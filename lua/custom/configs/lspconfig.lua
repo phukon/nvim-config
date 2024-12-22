58ef43b0b011512c2d6f0916a2b87db19f000c3d
phukon <hello@rikiphukon.com>
2024-12-22 21:31:19 +0530

    fix: multiple different client offset_encodings error with clang attaching to the current buffer

--- lua/custom/configs/lspconfig.lua
+++ lua/custom/configs/lspconfig.lua
@@ -34,7 +34,24 @@ lspconfig.gopls.setup({
 lspconfig.clangd.setup {
   on_attach = function (client, bufnr)
     client.server_capabilities.signatureHelpProvider = false
-    on_attach(client, bufnr)
+    on_attach(client, bufnr),
+  cmd = {
+    "clangd",
+    -- "--all-scopes-completion",
+    -- "--suggest-missing-includes",
+    -- "--background-index",
+    -- "--pch-storage=disk",
+    -- "--cross-file-rename",
+    -- "--log=info",
+    -- "--completion-style=detailed",
+    -- "--enable-config", -- clangd 11+ supports reading from .clangd configuration file
+    -- "--clang-tidy",
+    "--offset-encoding=utf-16",
+    -- "--clang-tidy-checks=-*,llvm-*,clang-analyzer-*,modernize-*,-modernize-use-trailing-return-type",
+    -- "--fallback-style=Google",
+    -- "--header-insertion=never",
+    -- "--query-driver=<list-of-white-listed-complers>"
+  },
   end,
   capabilities = capabilities,
 }
