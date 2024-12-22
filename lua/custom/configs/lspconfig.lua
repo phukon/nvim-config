a238b170c70c5c51d4e9d01f8db547869b0e85dd
phukon <hello@rikiphukon.com>
2024-12-22 22:41:57 +0530

    fix(lsp): set offset encoding during client attachment

--- lua/custom/configs/lspconfig.lua
+++ lua/custom/configs/lspconfig.lua
@@ -34,25 +34,26 @@ lspconfig.gopls.setup({
 lspconfig.clangd.setup {
   on_attach = function(client, bufnr)
     client.server_capabilities.signatureHelpProvider = false
+    client.offset_encoding = "utf-16"
     on_attach(client, bufnr)
   end,
-  cmd = {
-    "clangd",
-    -- "--all-scopes-completion",
-    -- "--suggest-missing-includes",
-    -- "--background-index",
-    -- "--pch-storage=disk",
-    -- "--cross-file-rename",
-    -- "--log=info",
-    -- "--completion-style=detailed",
-    -- "--enable-config", -- clangd 11+ supports reading from .clangd configuration file
-    -- "--clang-tidy",
-    "--offset-encoding=utf-16",
-    -- "--clang-tidy-checks=-*,llvm-*,clang-analyzer-*,modernize-*,-modernize-use-trailing-return-type",
-    -- "--fallback-style=Google",
-    -- "--header-insertion=never",
-    -- "--query-driver=<list-of-white-listed-complers>"
-  },
+  -- cmd = {
+  --   "clangd",
+  --   -- "--all-scopes-completion",
+  --   -- "--suggest-missing-includes",
+  --   -- "--background-index",
+  --   -- "--pch-storage=disk",
+  --   -- "--cross-file-rename",
+  --   -- "--log=info",
+  --   -- "--completion-style=detailed",
+  --   -- "--enable-config", -- clangd 11+ supports reading from .clangd configuration file
+  --   -- "--clang-tidy",
+  --   "--offset-encoding=utf-16",
+  --   -- "--clang-tidy-checks=-*,llvm-*,clang-analyzer-*,modernize-*,-modernize-use-trailing-return-type",
+  --   -- "--fallback-style=Google",
+  --   -- "--header-insertion=never",
+  --   -- "--query-driver=<list-of-white-listed-complers>"
+  -- },
   capabilities = capabilities
 }
 
